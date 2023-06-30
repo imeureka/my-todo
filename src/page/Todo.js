b57981646d3da66dee851551b9317da810353bc2
imeureka <lky062@naver.com>
2023-06-30 22:43:25 +0900

    feat: server.js 연결

--- src/page/Todo.js
+++ src/page/Todo.js
@@ -48,118 +48,119 @@ const fetchTodoList = async () => {
   };
 
 //todo 생성하기
-//   const handleAddTodo = async () => {
-//     try {
-//       const token = localStorage.getItem('token');
+  const handleAddTodo = async () => {
+    try {
+      const token = localStorage.getItem('token');
 
-//       const response = await axios.post('http://localhost:3001/todo', {
-//         title: newTodo,
-//       }, {
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-
-//       setTodos([...todos, response.data.todo]);
-//       setNewTodo('');
-//     } catch (error) {
-//       console.error('투두 추가 오류:', error);
-//     }
-//   };
-
-//todo page 내 그냥 생성하기
-const handleAddTodo = () => {
-    if (newTodo.trim() !== '') {
-      const todo = {
-        id: Date.now(),
+      const response = await axios.post('http://localhost:3001/todo', {
         title: newTodo,
-        completed: false,
-      };
-      setTodos([...todos, todo]);
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setTodos([...todos, response.data.todo]);
       setNewTodo('');
+    } catch (error) {
+      console.error('투두 추가 오류:', error);
     }
   };
 
-  const handleToggleComplete = (id) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, completed: !todo.completed };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-  };
-
-//   const handleToggleComplete = async (id) => {
-//     try {
-//       const token = localStorage.getItem('token');
-
-//       await axios.put(`http://localhost:3001/todo/${id}`, {
-//         completed: true,
-//       }, {
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-
-//       const updatedTodos = todos.map((todo) => {
-//         if (todo.id === id) {
-//           return { ...todo, completed: true };
-//         }
-//         return todo;
-//       });
-
-//       setTodos(updatedTodos);
-//     } catch (error) {
-//       console.error('투두 완료 표시 오류:', error);
+//todo page 내 그냥 생성하기
+// const handleAddTodo = () => {
+//     if (newTodo.trim() !== '') {
+//       const todo = {
+//         id: Date.now(),
+//         title: newTodo,
+//         completed: false,
+//       };
+//       setTodos([...todos, todo]);
+//       setNewTodo('');
 //     }
 //   };
 
-//    const handleDeleteTodo = async (id) => {
-//     try {
-//       const token = localStorage.getItem('token');
-
-//       await axios.delete(`http://localhost:3001/todo/${id}`, {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       });
-
-
-//       const updatedTodos = todos.filter((todo) => todo.id !== id);
-//       setTodos(updatedTodos);
-//     } catch (error) {
-//       console.error('투두 삭제 오류:', error);
-//     }
+//   const handleToggleComplete = (id) => {
+//     const updatedTodos = todos.map((todo) => {
+//       if (todo.id === id) {
+//         return { ...todo, completed: !todo.completed };
+//       }
+//       return todo;
+//     });
+//     setTodos(updatedTodos);
 //   };
 
-const handleDeleteTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+  const handleToggleComplete = async (id) => {
+    try {
+      const token = localStorage.getItem('token');
+
+      await axios.put(`http://localhost:3001/todo/${id}`, {
+        completed: true,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const updatedTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: true };
+        }
+        return todo;
+      });
+
+      setTodos(updatedTodos);
+    } catch (error) {
+      console.error('투두 완료 표시 오류:', error);
+    }
   };
 
-  const handleEditTodo = (id, title) => {
-    setEditTodoId(id);
-    setEditTodoText(title);
+   const handleDeleteTodo = async (id) => {
+    try {
+      const token = localStorage.getItem('token');
+
+      await axios.delete(`http://localhost:3001/todo/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+
+      const updatedTodos = todos.filter((todo) => todo.id !== id);
+      setTodos(updatedTodos);
+    } catch (error) {
+      console.error('투두 삭제 오류:', error);
+    }
   };
-  const handleUpdateTodo = (id) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, title: editTodoText };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-    setEditTodoId(null);
-    setEditTodoText('');
-  };
-//수정하기 -> 취소
-  const handleCancelEdit = () => {
-    setEditTodoId(null);
-    setEditTodoText('');
-  };
+
+
+// const handleDeleteTodo = (id) => {
+//     const updatedTodos = todos.filter((todo) => todo.id !== id);
+//     setTodos(updatedTodos);
+//   };
+
+//   const handleEditTodo = (id, title) => {
+//     setEditTodoId(id);
+//     setEditTodoText(title);
+//   };
+//   const handleUpdateTodo = (id) => {
+//     const updatedTodos = todos.map((todo) => {
+//       if (todo.id === id) {
+//         return { ...todo, title: editTodoText };
+//       }
+//       return todo;
+//     });
+//     setTodos(updatedTodos);
+//     setEditTodoId(null);
+//     setEditTodoText('');
+//   };
+// //수정하기 -> 취소
+//   const handleCancelEdit = () => {
+//     setEditTodoId(null);
+//     setEditTodoText('');
+//   };
 
   return (
     <div>
@@ -174,8 +175,8 @@ const handleDeleteTodo = (id) => {
                 value={editTodoText}
                 onChange={(e) => setEditTodoText(e.target.value)}
               />
-              <button onClick={() => handleUpdateTodo(todo.id)}>Save</button>
-              <button onClick={handleCancelEdit}>Cancel</button>
+              {/* <button onClick={() => handleUpdateTodo(todo.id)}>Save</button> */}
+              {/* <button onClick={handleCancelEdit}>Cancel</button> */}
             </>
           ) : (
             <>
@@ -187,7 +188,7 @@ const handleDeleteTodo = (id) => {
                 />
                 <span>{todo.title}</span>
               </label>
-              <button onClick={() => handleEditTodo(todo.id, todo.title)}>Edit</button>
+              {/* <button onClick={() => handleEditTodo(todo.id, todo.title)}>Edit</button> */}
               <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
             </>
           )}
